81373c1d9c7a0cd3c4d734a42bb0c3cd9ee37cee
GuiGui <bague.guillaume@live.fr>
2020-04-20 10:56:36 +0200

    add DELETE/:id route

--- app.js
+++ app.js
@@ -57,4 +57,11 @@ app.put(('/api/stuff/:id'), (req, res, next) => {
     .catch( error => res.status(400).json({ error }));
 });
 
+// DELETE ROUTE ID
+app.delete(('/api/stuff/:id'), (req, res, next) => {
+    Thing.deleteOne({ _id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Object Deleted !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
 module.exports = app;
