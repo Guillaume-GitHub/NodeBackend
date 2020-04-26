e4fa79a34d6f2a55e1b5ec9636f5590ed109ff01
GuiGui <bague.guillaume@live.fr>
2020-04-26 14:22:24 +0200

    Create User model with unique email validation
    use mongoose-unique-validator

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const uniqValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+    email = { type: String, required: true, unique: true },
+    password = { type: String, required: true }
+});
+
+// Use schema validator plugin (for unique email use)
+userSchema.plugin(userSchema);
+
+module.exports = mongoose.model('User', userSchema);
