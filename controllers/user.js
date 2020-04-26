c2cd6461c16aa9b97add78dc6df7468081da2da0
guillaume <bague.guillaume@live.fr>
2020-04-26 14:50:51 +0200

    Signup Method
    Use Bcryct to encrypt user password

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,19 @@
-exports.signup = (req, res, next) => {
+const User = require('..models/User');
+const bcryt = require('bcrypt');
 
+exports.signup = (req, res, next) => {
+    bcryt.hash(req.boby.password, 10)
+    .then(hash => {
+        const user =  User({
+            email: req.body.email,
+            password: hash
+        });
+
+        user.save()
+        .then(() => res.status(201).json({ message: 'User created !'}))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
 
 exports.login = (req, res, next) => {
